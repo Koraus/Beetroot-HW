9bb7c5fb697a9bf75b829ae7512ea50d8db8714d
Koraus <dmytro.kashchynets@gmail.com>
2021-11-03 12:08:41 +0200

    upd

---
+++ 25-Introducing-JS/js/add.js
@@ -0,0 +1,17 @@
+
+
+let a = 'Dmytro';
+console.log(a);
+
+function hello (){
+
+     if (document.querySelector('h1').innerHTML === 'JS - вступ') {
+        document.querySelector('h1').innerHTML = 'JS - вступ розпочато'   
+        
+    }
+    else {
+
+        (document.querySelector('h1').innerHTML = 'JS - вступ')
+    }
+    
+} 
